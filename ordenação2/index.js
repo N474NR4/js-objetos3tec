2c59f11747433d0f0e31b1a7a9ef11069dbd6a1b
N474NR4 <natan.ryzy.nascimento@escola.pr.gov.br>
2026-07-30 14:22:13 +0000

    arrays 1 e 2

---
+++ ordenação2/index.js
@@ -0,0 +1,12 @@
+const {edFolha, edGalho} =require('./arrays');
+
+function juntaListas(lista1, lista2){
+    let listaFinal = [];
+    let posiçãoAtualLista1 = 0;
+    let posiçãoAtualLista2 = 0;
+
+    while (posiçãoAtualLista1 < lista1.length edGalho && posiçãoAtualLista2 < lista2.length){
+        let produtoAtualLista1 = lista1[posiçãoAtualLista1];
+        let produtoAtualLista2 = lista2[posiçãoAtualLista2];
+    }
+}
